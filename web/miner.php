c9522b093bcb85bd82d945837da3e8d5d35d8385
Grant Emsley <grant@emsley.ca>
2017-12-23 18:23:27 -0500

    Allow use of POST instead of GET

--- web/miner.php
+++ web/miner.php
@@ -1,11 +1,11 @@
 <?php
-if(empty($_GET['address'])) { echo "Failure"; exit; }
-if(empty($_GET['workername'])) { echo "Failure"; exit; }
+if(empty($_REQUEST['address'])) { echo "Failure"; exit; }
+if(empty($_REQUEST['workername'])) { echo "Failure"; exit; }
 
 include('../functions.php');
-if(empty($_GET['miners'])) { $_GET['miners'] = ''; }
-if(empty($_GET['profit'])) { $_GET['profit'] = 0; }
+if(empty($_REQUEST['miners'])) { $_REQUEST['miners'] = ''; }
+if(empty($_REQUEST['profit'])) { $_REQUEST['profit'] = 0; }
 
-update_worker($_GET['address'], $_GET['workername'], $_GET['miners'], $_GET['profit']);
+update_worker($_REQUEST['address'], $_REQUEST['workername'], $_REQUEST['miners'], $_REQUEST['profit']);
 echo "Success";
 ?>
