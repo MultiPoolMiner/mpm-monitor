1a05630a03796471a195cf564475114d3feaa727
Grant Emsley <grant@emsley.ca>
2018-01-08 11:17:17 -0500

    Fix statusurl when running in subdirectory

--- web/stats.php
+++ web/stats.php
@@ -6,8 +6,15 @@ if(empty($_GET['address'])) {
 } else {
   require dirname(__FILE__).'/private/functions.php';
   $workers = get_workers_json($_GET['address']);
+
   if(empty(json_decode($workers, true))) { 
-    $statusurl = ( (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . 'miner.php';
+    $statusurl = ( (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'];
+    if(dirname($_SERVER['PHP_SELF']) == "/") {
+      $statusurl .= "/miner.php";
+    } else {
+      $statusurl .= dirname($_SERVER['PHP_SELF']) . 'miner.php';
+    }
+
     $workers = json_encode(array("error" => "Error: No workers found. Make sure your miner status URL is set to: $statusurl"));
   }
 }
