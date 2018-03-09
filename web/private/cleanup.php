cb2eaabda25b623481e867673bda9b59c0c65056
Grant Emsley <grant@emsley.ca>
2018-03-09 17:25:43 -0500

    Note about cron scheduling

--- web/private/cleanup.php
+++ web/private/cleanup.php
@@ -1,4 +1,6 @@
 <?php
+// This file is intended to be run as a scheduled task/cron job on your server.
+// It removes miners that have not been seen in the last 7 days.
 
 require dirname(__FILE__).'/functions.php';
 cleanup();
